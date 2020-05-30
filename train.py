c98b14c4b04be53b26a21cbc0825f0c16baa28b3
Jihwan Lim <ghwn5936@gmail.com>
2020-05-31 00:57:37 +0900

    Use CNN model

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import argparse
 
 import tensorflow as tf
+import numpy as np
 
 
 def train(train_batch_size, test_batch_size, epochs, output_dir):
@@ -8,11 +9,17 @@ def train(train_batch_size, test_batch_size, epochs, output_dir):
     
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train, x_test = x_train / 255.0, x_test / 255.0
+    x_train = np.expand_dims(x_train, axis=-1)
+    y_train = np.expand_dims(y_train, axis=-1)
 
     model = tf.keras.models.Sequential([
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)),
+        tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D((2, 2)),
+        tf.keras.layers.Dropout(0.25),
+        tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation='relu'),
-        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(10, activation='softmax'),
     ])
 
